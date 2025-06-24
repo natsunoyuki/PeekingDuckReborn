6795450dd10ceb3959f60ebbae97de2f406f93ce
Ненавин <33706936+natsunoyuki@users.noreply.github.com>
2025-06-24 15:10:01 +0800

    Updated licensing language for peekingduck/pipeline/nodes/model.

--- peekingduck/pipeline/nodes/model/jdev1/__init__.py
+++ peekingduck/pipeline/nodes/model/jdev1/__init__.py
@@ -1,4 +1,19 @@
-# Copyright 2022 AI Singapore
+# Modifications copyright 2025 Natsunoyuki AI Laboratory
+#
+# PeekingDuckReborn is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+#
+# PeekingDuckReborn is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# PeekingDuckReborn. If not, see <https://www.gnu.org/licenses/>.
+
+# Original copyright 2022 AI Singapore
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
