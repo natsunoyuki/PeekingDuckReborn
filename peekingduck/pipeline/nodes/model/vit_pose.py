1409c4f57ba0b9f947475bd3f066863e7163e2fb
Ненавин <33706936+natsunoyuki@users.noreply.github.com>
2025-06-27 15:44:29 +0800

    First implementation of VITPose pose detection model.

---
+++ peekingduck/pipeline/nodes/model/vit_pose.py
@@ -0,0 +1,104 @@
+# Copyright 2025 Natsunoyuki AI Laboratory
+#
+# PeekingDuckReborn is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+#
+# PeekingDuckReborn is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# PeekingDuckReborn. If not, see <https://www.gnu.org/licenses/>.
+
+"""🔲 VITpose pose estimation model."""
+
+from typing import Any, Dict, Optional
+
+from peekingduck.pipeline.nodes.abstract_node import AbstractNode
+from peekingduck.pipeline.nodes.model.vit_posev1.vit_pose_model import VITPoseModel
+
+
+class Node(AbstractNode):  # pylint: disable=too-few-public-methods
+    """Initializes and uses VITPose to infer keypoints from an image frame.
+
+    The VITPose node is capable of detecting keypoints from bounding boxes.
+    The ``"vitpose-plus-small"`` model is used by default and can be changed to 
+    one of ``("vitpose-plus-small", "vitpose-plus-base", "vitpose-plus-large", 
+    "vitpose-plus-huge", "vitpose-base", "vitpose-base-simple", 
+    "vitpose-base-coco-aic-mpii")``.
+
+    Inputs:
+        |img_data|
+
+        |bboxes_data|
+
+    Outputs:
+        |keypoints_data|
+
+        |keypoint_scores_data|
+
+        |keypoint_conns_data|
+
+    Configs:
+        model_format (:obj:`str`): **{"pytorch"},
+            default="pytorch"** |br|
+            Defines the weights format of the model.
+        model_type (:obj:`str`): **{"vitpose-plus-small", "vitpose-plus-base",
+            "vitpose-plus-large", "vitpose-plus-huge", "vitpose-base", 
+            "vitpose-base-simple", "vitpose-base-coco-aic-mpii"}, 
+            default="vitpose-plus-small"**. |br|
+            Defines the type of VITPose model to be used.
+        weights_parent_dir (:obj:`Optional[str]`): **default = null**. |br|
+            Change the parent directory where weights will be stored by
+            replacing ``null`` with an absolute path to the desired directory.
+        resolution (:obj:`dict[int, int]`): 
+            **default={width: 256, height: 192}**. |br|
+            Input image resolution of the VITPose model.
+        score_threshold (:obj:`float`): **[0, 1], default = 0.25**. |br|
+            Keypoints with confidence score below the threshold will be
+            discarded.
+
+    References:
+        ViTPose: Simple Vision Transformer Baselines for Human Pose Estimation:
+        https://huggingface.co/docs/transformers/v4.48.2/model_doc/vitpose
+
+        Implementation on HuggingFace:
+        https://huggingface.co/docs/transformers/v4.48.2/model_doc/vitpose
+
+        Inference code and model weights:
+        ViTPose: Simple Vision Transformer Baselines for Human Pose Estimation
+    """
+    def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
+        super().__init__(config, node_path=__name__, **kwargs)
+        self.model = VITPoseModel(self.config)
+
+
+    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        """Reads the bbox input and returns the poses and pose bbox of the
+        specified objects chosen to be detected.
+        """
+        keypoints, keypoint_scores, keypoint_conns = self.model.predict(
+            inputs["img"], inputs["bboxes"],
+        )
+        return {
+            "keypoints": keypoints,
+            "keypoint_scores": keypoint_scores,
+            "keypoint_conns": keypoint_conns,
+        }
+
+
+    def _get_config_types(self) -> Dict[str, Any]:
+        """
+        Returns dictionary mapping the node's config keys to respective types.
+        """
+        return {
+            "resolution.height": int,
+            "resolution.width": int,
+            "model_format": str,
+            "model_type": str,
+            "score_threshold": float,
+            "weights_parent_dir": Optional[str],
+        }
---
+++ peekingduck/pipeline/nodes/model/vit_posev1/vit_pose_model.py
@@ -0,0 +1,90 @@
+# Copyright 2025 Natsunoyuki AI Laboratory
+#
+# PeekingDuckReborn is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+#
+# PeekingDuckReborn is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# PeekingDuckReborn. If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+from typing import Any, Dict, List, Tuple
+
+import numpy as np
+
+from peekingduck.pipeline.nodes.base import (
+    ThresholdCheckerMixin,
+    WeightsDownloaderMixin,
+)
+from peekingduck.pipeline.nodes.model.vit_posev1.vit_pose_files.detector import Detector
+
+
+HF_REPO = "usyd-community"
+
+
+class VITPoseModel(ThresholdCheckerMixin, WeightsDownloaderMixin):
+    """Validates configuration, loads VITPose model, and performs inference.
+
+    Configuration options are validated to ensure they have valid types and
+    values. Model weights files are downloaded if not found in the location
+    indicated by the `weights_dir` configuration option.
+
+    Attributes:
+        detector (Detector): VITPose keypoint detector object to infer human
+            pose keypoints from a provided image frame.
+    """
+    def __init__(self, config: Dict[str, Any]) -> None:
+        self.config = config
+        self.logger = logging.getLogger(__name__)
+
+        self.check_bounds(["score_threshold"], "[0, 1]")
+
+        use_hf = self.config.get("huggingface", True)
+        if use_hf is True:
+            model_dir = self.config.get("huggingface_model_dir", HF_REPO)
+        else:
+            model_dir = self._find_paths()
+
+
+        self.detector = Detector(
+            model_dir,
+            self.config["model_type"],
+            self.config["resolution"],
+            self.config["score_threshold"],
+        )
+
+
+    def predict(
+        self, 
+        image: np.ndarray,
+        bboxes: np.ndarray,
+    ) -> Tuple[np.ndarray, np.ndarray, List]:
+        """Predicts keypoints from image and bboxes.
+
+        Args:
+            image (np.ndarray): Input image frame.
+            bboxes (np.ndarray): Person bounding boxes from an object detector.
+
+        Returns:
+            (Tuple[np.ndarray, np.ndarray, list]): Returned tuple
+            contains:
+            - An array of keypoints with shape [N, K, 2],
+            - An array of keypoint scores with shape [N, K],
+            - A list of length N of keypoint connections with shape [Dk, 2, 2],
+            where N is the number of persons, K is the number of keypoints which
+            is enforced to 17 following MSCOCO, and Dk is the number of valid
+            keypoint connections.
+
+        Raises:
+            TypeError: The provided `image` is not a numpy array.
+        """
+        if not isinstance(image, np.ndarray):
+            raise TypeError("image must be a np.ndarray")
+
+        return self.detector.predict_keypoints_from_image(image, bboxes)
